88b5e8aca3eb1fee33ae42c31204b0e847865779
rgarcia2304 <rgarcia22141@gmail.com>
2025-09-01 11:36:42 -0400

    added calculator

--- main.py
+++ main.py
@@ -5,19 +5,32 @@ import sys
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
 client = genai.Client(api_key=api_key)
+from google.genai import types
 
 def main():
-
     
-    if len(sys.argv) != 2:
+   
+    
+    if len(sys.argv) < 2:
         print("No prompt was entered")
         return 1
     else:
+
+        messages = [
+                 types.Content(role="user", parts=[types.Part(text=sys.argv[1])]),
+                 ]
+
         response = client.models.generate_content(
-        model='gemini-2.0-flash-001', contents=sys.argv[1])
-        print(response.text)
-        print(f'Prompt Tokens: {response.usage_metadata.prompt_token_count}')
-        print(f'Response Tokens: {response.usage_metadata.candidates_token_count}')
+                model='gemini-2.0-flash-001', 
+                contents=messages,
+                )
+        if len(sys.argv) == 3:
+            if sys.argv[2] == "--verbose":
+                print(f"User prompt: {sys.argv[1]}")
+                print(f'Prompt Tokens: {response.usage_metadata.prompt_token_count}')
+                print(f'Response Tokens: {response.usage_metadata.candidates_token_count}')
+        
+        print(response.text)    
         return 0
 
 if __name__ == "__main__":
