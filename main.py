a2dcab33f20e589859d84a296be80a5168985f5c
rgarcia2304 <rgarcia22141@gmail.com>
2025-09-01 11:05:29 -0400

    Have command like support for prompting questions

--- main.py
+++ main.py
@@ -1,6 +1,24 @@
-def main():
-    print("Hello from ai-agent!")
+import os
+from dotenv import load_dotenv
+from google import genai
+import sys
+load_dotenv()
+api_key = os.environ.get("GEMINI_API_KEY")
+client = genai.Client(api_key=api_key)
 
+def main():
+
+    
+    if len(sys.argv) != 2:
+        print("No prompt was entered")
+        return 1
+    else:
+        response = client.models.generate_content(
+        model='gemini-2.0-flash-001', contents=sys.argv[1])
+        print(response.text)
+        print(f'Prompt Tokens: {response.usage_metadata.prompt_token_count}')
+        print(f'Response Tokens: {response.usage_metadata.candidates_token_count}')
+        return 0
 
 if __name__ == "__main__":
     main()
